1a066cfae213bc7aef20bfbbc3c060c247d2b73c
Robert Brenckman <20431767+RFBomb@users.noreply.github.com>
2025-12-07 14:03:53 -0500

    Week 6

---
+++ ITE215/Week 6 - C# Fundamentals/ECommerceData/DTOs.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ECommerceData
+{
+    public record Customer(int Id, string EmailAddress) { }
+
+    public record Product(int Id, string Description, string Category, double Price) { }
+
+    /// <summary>
+    /// Represents a line item on an order
+    /// </summary>
+    public record LineItem(Product Product, int Quantity) { public double GetSubTotal() => Product.Price * Quantity; }
+
+    public interface IOrder
+    {
+        Guid OrderId { get; }
+        int CustomerId { get; }
+        IEnumerable<LineItem> LineItems { get; }
+        double GetSubTotal() => LineItems.Select(p => p.Product.Price * p.Quantity).Sum();
+    }
+
+    /// <summary>
+    /// Represents a Cart that the user can populate with items
+    /// </summary>
+    /// <param name="CartId"></param>
+    public record Cart(Guid CartId) : IOrder
+    {
+        private readonly Dictionary<Product, int> cart = [];
+        private int customerId = -1;
+
+        internal Cart() : this(Guid.NewGuid()) { }
+
+        [JsonConstructor]
+        internal Cart(Guid CartId, int CustomerId, IEnumerable<LineItem> lineItems) : this(CartId)
+        {
+            customerId = CustomerId;
+            foreach (var item in lineItems)
+                cart[item.Product] = item.Quantity;
+        }
+
+
+        // allow setting the customerID, but not changing it
+        public int CustomerId
+        {
+            get => customerId;
+            set
+            {
+                if (customerId < 0 && value >= 0)
+                    customerId = value;
+                else if (value >= 0 && customerId != value)
+                    throw new InvalidOperationException("Cannot change the owner of a cart");
+            }
+        }
+
+        Guid IOrder.OrderId => this.CartId;
+
+        public IEnumerable<LineItem> LineItems => cart.Select(kvp => new LineItem(kvp.Key, kvp.Value));
+
+        public double GetSubTotal() => cart.Select(p => p.Key.Price * p.Value).Sum();
+
+        public void UpdateCart(Product product, int quantityToAdd)
+        {
+            bool found = cart.TryGetValue(product, out int value);
+            int newValue = value + quantityToAdd;
+            if (newValue > 0)
+                cart[product] = newValue;
+            else if (found)
+                cart.Remove(product);
+        }
+
+        public void RemoveFromCart(Product product) => cart.Remove(product);
+        public void SetQuantity(Product product, int quantity) => cart[product] = quantity;
+    }
+
+    /// <summary>
+    /// Represents an order that has been placed
+    /// </summary>
+    /// <param name="OrderId"></param>
+    /// <param name="CustomerId"></param>
+    public record Order(Guid OrderId, int CustomerId, DateTime OrderDate, IEnumerable<LineItem> LineItems) : IOrder
+    {
+        internal Order(Cart cart) : this(cart.CartId, cart.CustomerId, DateTime.UtcNow, cart.LineItems) { }
+    }
+}
---
+++ ITE215/Week 6 - C# Fundamentals/ECommerceData/ECommerceSystem.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ECommerceData
+{
+    /// <summary>
+    /// The ECommerse Management System
+    /// </summary>
+    public partial class ECommerceSystem
+    {
+        [JsonSourceGenerationOptions(AllowTrailingCommas =true, WriteIndented = true)]
+        [JsonSerializable(typeof(ECommerceSystem))]
+        public partial class SerializerContext : JsonSerializerContext { }
+
+        [JsonInclude] private HashSet<Product> Products { get; init; } = [];
+        [JsonInclude] private Dictionary<string, Customer> Customers { get; init; } = [];
+        [JsonInclude] private List<Order> OrderHistory { get; init; } = [];         // placed orders
+        [JsonInclude] private Dictionary<int, Cart> Carts { get; init; } = [];     // active carts, where key = customerId
+
+        #region < Product Management >
+
+        public void AddProduct(string? descrip, string? category, double price)
+        {
+            Products.Add(new Product(Products.Count, descrip!, category!, price));
+        }
+
+        public IEnumerable<Product> GetProducts() => Products;
+
+        #endregion
+
+        #region < Customers >
+
+        public Customer RegisterCustomer(string emailAddress)
+        {
+            // pretend we also do email validation here
+            if (Customers.TryGetValue(emailAddress.ToLowerInvariant(), out _))
+                throw new ArgumentException("Email Address already Taken");
+            var cust = new Customer(Customers.Count + 1, emailAddress);
+            Customers[cust.EmailAddress.ToLowerInvariant()] = cust;
+            return cust;
+        }
+        public Customer? LoginCustomer(string emailAddress)
+        {
+            Customers.TryGetValue(emailAddress.ToLowerInvariant(), out var result);
+            return result;
+        }
+
+        public Cart GetCustomerCart(Customer customer) => GetCustomerCart(customer.Id);
+        public Cart GetCustomerCart(int customerId = -1)
+        {
+            if (Carts.TryGetValue(customerId, out var cart)) return cart;
+            cart = new Cart(Guid.NewGuid()) { CustomerId = customerId };
+            if (customerId >= 0) Carts[customerId] = cart;
+            return cart;
+        }
+        
+        public IEnumerable<Order> GetCustomerOrders(Customer customer) => GetCustomerOrders(customer.Id);
+        public IEnumerable<Order> GetCustomerOrders(int customerId) => OrderHistory.Where(o => o.CustomerId == customerId);
+
+        public Cart PlaceOrder(Cart cart, int customerId)
+        {
+            // Validation
+            ArgumentNullException.ThrowIfNull(cart);
+            ArgumentOutOfRangeException.ThrowIfLessThan(customerId, 0, nameof(customerId));
+            cart.CustomerId = customerId;
+
+            // generate a new cart for the dictionary to avoid adding items to this cart
+            var newCart = new Cart() { CustomerId = customerId };
+            Carts[customerId] = newCart;
+
+            // convert the cart to an order history
+            OrderHistory.Add(new Order(cart));
+
+            // return the new (empty) cart
+            return newCart; 
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Prints invoices to text files
+        /// </summary>
+        public void PrintInvoice(IOrder order, Customer? customer)
+        {
+            if (order == null) return;
+            //Directory.CreateDirectory(".\\Invoices\\");
+            //var fp = Path.Combine(".\\Invoices\\", order.OrderId.ToString());
+            //using var stream = File.OpenWrite(fp);
+            //using var writer = new StreamWriter(stream, UTF8Encoding.UTF8);
+            
+
+            customer ??= Customers.Where(c => c.Value.Id == order.CustomerId).Select(s => s.Value).FirstOrDefault();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"-------------------------------------------------");
+            Console.WriteLine($"Customer ID: {customer?.EmailAddress}");
+            if (order is Order o)
+            {
+                Console.WriteLine($"Order ID   : {order.OrderId}");
+                Console.WriteLine($"Order Date : {o.OrderDate}");
+            }
+            else // is a cart
+            {
+                Console.WriteLine($"Cart ID : {order.OrderId}");
+                Console.WriteLine($"Date    : {DateTime.UtcNow}");
+            }
+            Console.WriteLine($"-------------------------------------------------");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{"Description",-20}\t{"Qty", 10}\tCost");
+            Console.ForegroundColor = ConsoleColor.White;
+            double total = 0;
+            foreach (var item in order.LineItems)
+            {
+                double subTotal = item.GetSubTotal();
+                total += subTotal;
+                Console.WriteLine($"{item.Product.Description,-20}\t{item.Quantity, 10}\t{subTotal:f2}");
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"-------------------------------------------------");
+            Console.WriteLine($"\t\t\t Subtotal : ${total:f2}");
+            Console.WriteLine($"-------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.White;
+            //writer.Flush();
+            //writer.Close();
+            //writer.Dispose();
+            //stream.Dispose();
+        }
+
+        
+
+        private const string fp = ".\\ECommerceData.json";
+
+        public void SaveToDisk()
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(Path.GetFullPath(fp))!);
+            using var outStream = File.OpenWrite(fp);
+            JsonSerializer.Serialize(outStream, this, SerializerContext.Default.ECommerceSystem);
+        }
+
+        public static ECommerceSystem LoadFromDisk()
+        {
+            if (File.Exists(fp))
+            {
+                using var reader = File.OpenRead(fp);
+                return JsonSerializer.Deserialize(reader, SerializerContext.Default.ECommerceSystem) ?? new();
+            }
+            else
+            {
+                return new ECommerceSystem();
+            }
+        }
+
+    }
+}
